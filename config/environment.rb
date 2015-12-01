74bf2c5e1f67e11396ebf2fcaebad32142c655dd
Felipe Henao <fahenao@MacBook-Pro-de-Felipe.local>
2015-12-01 17:17:19 -0500

    msg

--- config/environment.rb
+++ config/environment.rb
@@ -5,42 +5,12 @@ require 'mail'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Mail.defaults do
-  delivery_method :smtp, {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
-end
-
-mail = Mail.deliver do
-		@url = 'www.jwseguros.com'
-		to 'cotizacion@jwseguros'
-	  from    'JW Seguros <cotizacion@jwseguros.com>'
-	  subject 'Cotizacion de su poliza - JW Seguros'
-
-	  text_part do
-	    body 'Hola, <%= Client.last.fullname %>
-						===============================================
-						Gracias por confiar en nosotros, <%= Client.last.fullname %>
-						 
-						En breves momentos lo estaremos contactando.
-						    
-						Si desea cotizar otro tipo de poliza puede haga click en el siguiente link: <%= @url %>.'
-	  end
-
-	  html_part do
-	    content_type 'text/html; charset=UTF-8'
-	    body 				'<h1>Gracias por confiar en nosotros, <%= Client.last.fullname %></h1>
-	      		      <p>
-	      		        En breves momentos lo estaremos contactando.<br>
-	      		      </p>
-	      		      <p>
-	      		        Si desea cotizar otro tipo de poliza puede haga click en el siguiente link: <%= @url %>.
-	      		      </p>'
-	  end
-	end
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
