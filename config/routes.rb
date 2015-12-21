8269a26945d2c9a83445ed933cfee1b2dc8ee84d
Felipe Henao <fahenao@MBP-de-Felipe.cpe.cableonda.net>
2015-12-21 13:06:58 -0500

    Auto form year range modifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'clients#index'
 
-  resources :cars
-  resources :clients
-  resources :sureties
-  resources :fires
-  resources :lives
-  resources :healths
-  resources :contents
+  resources :cars, except: [:update, :destroy]
+  resources :clients, except: [:update, :destroy]
+  resources :sureties, except: [:update, :destroy]
+  resources :fires, except: [:update, :destroy]
+  resources :lives, except: [:update, :destroy]
+  resources :healths, except: [:update, :destroy]
+  resources :contents, except: [:update, :destroy]
 
 end
